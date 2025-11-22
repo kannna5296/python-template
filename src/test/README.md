32919620e1bfdc5d8e1a7a0f032650bb40c58caf
kannna5296 <kannnahacks5296@gmail.com>
2025-11-22 12:36:52 +0900

    独自のファイル修正

--- src/test/README.md
+++
@@ -1,5 +0,0 @@
- Effective Python 3の13章（テストのところ）をいろいろしようとする
-
-* mock_playground -> まずmockに触れる
-* simple_test -> とりあえずテスト書いてみる
-* better_test -> カプセル化してテスト書きやすくする
--- src/test/mock_playground/database.py
+++
@@ -1,9 +0,0 @@
-class DatabaseConnection:
-    def __init__(self, url: str, port: int):
-        self.url = url
-        self.port = port
-
-
-# テスト対象を想定
-def get_annimals(database: DatabaseConnection, species: str):
-    pass
